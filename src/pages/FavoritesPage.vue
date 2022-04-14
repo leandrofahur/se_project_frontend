af9c7af91d313839be53a380b5e88dbdf09e1b45
Leandro Machado <fahurleandro@gmail.com>
2022-04-14 14:00:38 -0700

    update favorite page

---
+++ src/pages/FavoritesPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <NavbarComponent />
+  <FooterComponent />
+</template>
+
+<script>
+import NavbarComponent from "../components/NavbarComponent.vue";
+import FooterComponent from "../components/FooterComponent.vue";
+
+export default {
+  name: "FavoritesPage",
+  components: {
+    NavbarComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+<style scoped></style>
